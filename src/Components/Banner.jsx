725a3786526787e799c956eb59dfd88ab03419b8
Console.log("Pinaki") <Pinaki.bpd2001@gmail.com>
2024-08-05 12:04:40 +0530

    banner section

---
+++ src/Components/Banner.jsx
@@ -0,0 +1,91 @@
+import React, { useEffect, useState } from "react";
+import { CiCircleChevLeft, CiCircleChevRight } from "react-icons/ci";
+
+const Banner = ({ BASE_URL, API_KEY }) => {
+  const [trendingLists, setTrendingLists] = useState([]);
+
+  const trendingShows = async () => {
+    try {
+      const response = await fetch(
+        `${BASE_URL}trending/all/week?api_key=${API_KEY}`
+      );
+      const data = await response.json();
+      setTrendingLists(data.results);
+    } catch (error) {
+      console.log({ message: error.message });
+    }
+  };
+
+  useEffect(() => {
+    trendingShows();
+  }, []);
+
+  const leftScroll = () => {
+    const BannerContainer = document.querySelector(".BannerContainer");
+    const bannerWidth = BannerContainer.offsetWidth;
+    BannerContainer.scrollBy({ left: -bannerWidth, behavior: "smooth" });
+  };
+
+  const rightScroll = () => {
+    const BannerContainer = document.querySelector(".BannerContainer");
+    const bannerWidth = BannerContainer.offsetWidth;
+    BannerContainer.scrollBy({ left: bannerWidth, behavior: "smooth" });
+  };
+
+  return (
+    <section className="relative h-screen">
+      <i
+        onClick={leftScroll}
+        className="absolute top-[50%] text-white text-5xl z-10 cursor-pointer"
+      >
+        <CiCircleChevLeft />
+      </i>
+      <div className="BannerContainer overflow-x-auto whitespace-nowrap h-full">
+        {trendingLists.map((Element, id) => {
+          return (
+            <div key={id} className="inline-block relative h-full">
+              {/* Banner image */}
+              <img
+                src={`https://image.tmdb.org/t/p/original/${Element.backdrop_path}`}
+                alt=""
+                className="w-screen h-full object-cover"
+              />
+              {/* Banner data */}
+              <div className="bannerData absolute top-0 h-screen left-0 w-full text-white flex items-center">
+                <div className="pl-20">
+                  {/* Title of the show */}
+                  <h2 className="text-4xl font-bold">
+                    {Element.title || Element.name}
+                  </h2>
+                  {/* Release date and type */}
+                  <div className="flex gap-3 my-2">
+                    <p>{Element.media_type}</p>
+                    <p>{`${Element.release_date}`.substring(0, 4)}</p>
+                  </div>
+                  {/* Overview of the show */}
+                  <div className="overview w-[40%] h-[12rem] overflow-hidden mt-3">
+                    <p className="line-clamp text-[#e5e5e5] font-semibold">
+                      {`${Element.overview}`.length > 412 ? `${Element.overview}`.substring(0, 411) + '...more' : Element.overview}
+                    </p>
+                  </div>
+                  <div className="buttons flex gap-3">
+                    <button className="rounded bg-amber-500 w-60 px-5 py-2 text-black font-semibold" >Suscribe to watch</button>
+                    <button className="rounded bg-amber-500 w-12 px-5 py-2 text-black font-semibold" >+</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+      <i
+        onClick={rightScroll}
+        className="absolute top-[50%] text-white text-5xl right-0 z-10 cursor-pointer"
+      >
+        <CiCircleChevRight />
+      </i>
+    </section>
+  );
+};
+
+export default Banner;
--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import Banner from './Banner'
 
 const Main = () => {
+
+    const BASE_URL = 'https://api.themoviedb.org/3/'
+    const API_KEY = '376a7b56bf7b28c1457c230c2ebbd63a'
+
   return (
     <div className='pl-[10vw]' >
-      This is main tab
+      <Banner BASE_URL={BASE_URL} API_KEY = {API_KEY} />
     </div>
   )
 }
